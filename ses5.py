b685b99d7d5c60479b763a6f2268e6b710f6fc76
Abrahan-Perez <i2511689@continental.edu.pe>
2025-03-06 22:05:34 -0500

    Ejemplo con while

--- ses5.py
+++ ses5.py
@@ -1,1 +1,16 @@
-print('Hola mundo')
+import streamlit as st
+
+def main():
+    st.title("Ejemplo de while y for")
+    
+    #Ejemplo de while
+    st.subheader('Ejemplo de while')
+    n = st.number_input('Ingresa un número para contar hasta 5: '), min_value=0, max_value=5,
+    contador = 0
+    resultado_while = ""
+    
+    while contador <= n:
+        resultado_while += f"{contador}"
+        contador += 1
+    st.write('Secuencia generada con while: ', resultado_while)
+    
